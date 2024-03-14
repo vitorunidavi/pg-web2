95ef9fc01d7590e64c835994ac9a0ae732c2d935
vitor hugo tavares <vitor.tavares@unidavi.edu.br>
2024-03-14 14:26:33 -0300

    ok

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Minha Aplicação</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Minha Aplicação</h1>
+    <p>Bem-vindo à minha aplicação PHP com controle de versionamento!</p>
+</body>
+</html>
+<h1>teste</h1>
+alteração
